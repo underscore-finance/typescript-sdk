17a4f7cea9486512ff882267bcfa0d747592abf8
Mick Hagen <mickhagen@gmail.com>
2026-05-05 16:56:27 -0600

    Merge generated ABIs additively

--- dapp.config.js
+++ dapp.config.js
@@ -6,6 +6,14 @@ const ChequeBookAbi = require('../underscore-protocol/scripts/abis/ChequeBook.js
 const UserWalletConfigAbi = require('../underscore-protocol/scripts/abis/UserWalletConfig.json')
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { LevgVaultHelper, '40Acres': FortyAcres, Yo, Tokemak, ...rest } = Manifest.contracts
+
+function mergeAbi(baseAbi, extraAbi) {
+  const signature = (item) =>
+    `${item.type}:${item.name ?? ''}(${(item.inputs ?? []).map((input) => input.type).join(',')})`
+  const seen = new Set(baseAbi.map(signature))
+  return [...baseAbi, ...extraAbi.filter((item) => !seen.has(signature(item)))]
+}
+
 export default {
   name: 'Underscore Finance',
   description: 'Underscore Finance',
@@ -33,11 +41,11 @@ export default {
     UserWalletConfig: {
       isTemplate: true,
       address: Manifest.contracts.UserWalletConfig.address,
-      abi: UserWalletConfigAbi,
+      abi: mergeAbi(Manifest.contracts.UserWalletConfig.abi, UserWalletConfigAbi),
     },
     ChequeBook: {
       ...Manifest.contracts.ChequeBook,
-      abi: ChequeBookAbi,
+      abi: mergeAbi(Manifest.contracts.ChequeBook.abi, ChequeBookAbi),
     },
     UserWalletV1: {
       isTemplate: true,
